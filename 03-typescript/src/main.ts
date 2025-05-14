35a811ca79909362b5f615f8f2f1d0e57af49038
Paul Negoescu <paul.negoescu@nagarro.com>
2025-05-14 21:04:32 +0300

    End of first TS course

---
+++ 03-typescript/src/main.ts
@@ -0,0 +1,10 @@
+let a: boolean = true;
+let b: number = 1;
+let c: string = "Paul";
+let d: null = null;
+let e: undefined = undefined;
+let f: symbol = Symbol('Paul');
+let g: bigint = 2n;
+
+console.log(a, b, c, d, e, f, g);
+
