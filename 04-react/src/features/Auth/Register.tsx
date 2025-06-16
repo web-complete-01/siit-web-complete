d11ca3f0f609b056960e36bf243bf94aa5e019ec
Paul Negoescu <paul.negoescu@nagarro.com>
2025-06-16 18:36:52 +0300

    End of react course 6

--- 04-react/src/features/Auth/Register.tsx
+++ 04-react/src/features/Auth/Register.tsx
@@ -1,89 +1,130 @@
 import { useState } from 'react';
+import { z, type ZodObject } from 'zod/v4';
 
-export function Register() {
-  // Oldschool React way: controlled inputs (two-way binding)
-  // 1. state for the input
-  // 2. value of field === state of the input <input value={formValues.email}  />
-  // 3. field onChange modifies the state variable setFormValues({...formValues, email: e.target.value })
-  const [formValues, setFormValues] = useState({
+const validationSchema = z
+  .object({
+    email: z.email('Please enter a valid email address.'),
+    password: z
+      .string()
+      .min(6, 'Your password should be at least 6 characters long.'),
+    retypePassword: z
+      .string()
+      .min(6, 'Your password should be at least 6 characters long.'),
+    firstName: z.string().min(1, 'Please tell us your first name.'),
+    lastName: z.string().min(1, 'Please tell us your last name.'),
+  })
+  .refine((data) => data.password === data.retypePassword, {
+    message: 'The passwords did not match.',
+    path: ['retypePassword'],
+  });
+
+function validateForm<T extends ZodObject>(
+  formValues: Record<string, FormDataEntryValue>,
+  validationSchema: T
+) {
+  const result = validationSchema.safeParse(formValues);
+
+  if (result.error) {
+    return z.flattenError(result.error).fieldErrors;
+  }
+  return null;
+}
+
+type SchemaObject = z.infer<typeof validationSchema>;
+type ErrorObject = Record<keyof SchemaObject, string[]>;
+type Errors = Partial<ErrorObject>;
+
+
+const initialDefaultValues = {
     email: '',
     password: '',
     retypePassword: '',
     firstName: '',
     lastName: '',
-  });
+  };
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    console.log(formValues);
-  }
+export function Register() {
+  const [errors, setErrors] = useState<null | Errors>(null);
+  const [defaultValues, setDefaultValues] = useState(initialDefaultValues);
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+    if(!errors) {
+      return;
+    }
+    const formValues = new FormData(e.target.form!);
+    const newErrors = validateForm(Object.fromEntries(formValues.entries()), validationSchema)
+    // setErrors({...errors, [e.target.name]: null});
+    setErrors(newErrors);
   }
 
-  // classic JS way of handling a submit event
-  // function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-  //   e.preventDefault();
+  async function handleRegister(data: FormData) {
+    const values = Object.fromEntries(data.entries());
+    const errors = validateForm(values, validationSchema);
 
-  //   const data = new FormData(e.target as HTMLFormElement);
-  //   console.log(data.get('firstName'));
-  //   for(const value of data.entries()) {
-  //     console.log(value);
+    if (errors) {
+      setErrors(errors);
+      setDefaultValues(values as typeof defaultValues);
+      return;
+    }
 
-  //   }
-  // }
+    setErrors(null);
+    setDefaultValues(initialDefaultValues);
+  }
 
   return (
-    <form action="" className="brandForm" onSubmit={handleSubmit}>
+    <form action={handleRegister} className="brandForm" noValidate>
       <h1 className="fullWidth">Register</h1>
 
       <label htmlFor="email">Email</label>
       <input
-        value={formValues.email}
-        onChange={handleInputChange}
         type="email"
         name="email"
         id="email"
         data-input="email"
+        defaultValue={defaultValues.email}
+        onChange={handleInputChange}
       />
+      {errors?.email && <p className="fieldError">{errors.email[0]}</p>}
 
       <label htmlFor="password">Password</label>
       <input
-        value={formValues.password}
-        onChange={handleInputChange}
         type="password"
         name="password"
         id="password"
+        defaultValue={defaultValues.password}
+        onChange={handleInputChange}
       />
+      {errors?.password && <p className="fieldError">{errors.password[0]}</p>}
 
       <label htmlFor="retypePassword">Retype Password</label>
       <input
-        value={formValues.retypePassword}
-        onChange={handleInputChange}
         type="password"
         name="retypePassword"
         id="retypePassword"
+        defaultValue={defaultValues.retypePassword}
+        onChange={handleInputChange}
       />
+      {errors?.retypePassword && <p className="fieldError">{errors.retypePassword[0]}</p>}
 
       <label htmlFor="firstName">First Name</label>
       <input
-        value={formValues.firstName}
-        onChange={handleInputChange}
         type="text"
         name="firstName"
         id="firstName"
+        defaultValue={defaultValues.firstName}
+        onChange={handleInputChange}
       />
+      {errors?.firstName && <p className="fieldError">{errors.firstName[0]}</p>}
 
       <label htmlFor="lastName">Last Name</label>
       <input
-        value={formValues.lastName}
-        onChange={handleInputChange}
         type="text"
         name="lastName"
         id="lastName"
+        defaultValue={defaultValues.lastName}
+        onChange={handleInputChange}
       />
+      {errors?.lastName && <p className="fieldError">{errors.lastName[0]}</p>}
 
       <button type="submit" className="btn secondColumn">
         Register
